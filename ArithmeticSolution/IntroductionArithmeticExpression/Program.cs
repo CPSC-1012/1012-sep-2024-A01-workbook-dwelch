5c1823ca928e5c8b199059d0e4dec0fbcb6e92e3
Don Welch <dwelch@nait.ca>
2024-09-17 10:59:19 -0600

    demo math operators for integer and double

---
+++ ArithmeticSolution/IntroductionArithmeticExpression/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\n\tHello, World of Math Expression!\n\n");
+
+int intA = 3, intB = 4, intC = 0;
+
+//basic arithmetic operators
+
+//addition
+intC = intA + intB;
+Console.WriteLine($"\nThe result of the math expression {intA} + {intB} is: {intC }\n");
+
+//subtration
+intC = intA - intB;
+Console.WriteLine($"\nThe result of the math expression {intA} - {intB} is: {intC}\n");
+
+//Multipication
+intC = intA * intB;
+Console.WriteLine($"\nThe result of the math expression {intA} * {intB} is: {intC}\n");
+
+//Division
+intC = intA / intB;
+Console.WriteLine($"\nThe result of the math expression {intA} / {intB} is: {intC}");
+intC = intB / intA;
+Console.WriteLine($"\nThe result of the math expression {intB} / {intA} is: {intC}\n");
+
+//Modular Division
+//addition
+intC = intA % intB;
+Console.WriteLine($"\nThe result of the math expression {intA} % {intB} is: {intC}");
+intC = intB % intA;
+Console.WriteLine($"\nThe result of the math expression {intB} % {intA} is: {intC}\n");
+
+double doubleA = 3, doubleB = 4, doubleC = 0;
+
+//basic arithmetic operators
+
+//addition
+doubleC = doubleA + doubleB;
+Console.WriteLine($"\nThe result of the math expression {doubleA} + {doubleB} is: {doubleC}\n");
+
+//subtration
+doubleC = doubleA - doubleB;
+Console.WriteLine($"\nThe result of the math expression {doubleA} - {doubleB} is: {doubleC}\n");
+
+//Multipication
+doubleC = doubleA * doubleB;
+Console.WriteLine($"\nThe result of the math expression {doubleA} * {doubleB} is: {doubleC}\n");
+
+//Division
+doubleC = doubleA / doubleB;
+Console.WriteLine($"\nThe result of the math expression {doubleA} / {doubleB} is: {doubleC}");
+doubleC = doubleB / doubleA;
+Console.WriteLine($"\nThe result of the math expression {doubleB} / {doubleA} is: {doubleC}\n");
+
