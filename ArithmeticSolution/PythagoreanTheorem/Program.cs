7f940604f23a361ab6b594dd93404f898f000261
Don Welch <dwelch@nait.ca>
2024-09-20 13:47:01 -0600

    demo constants and incremental arithmetic code

--- ArithmeticSolution/PythagoreanTheorem/Program.cs
+++ ArithmeticSolution/PythagoreanTheorem/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("\n\tPythagorean Theorem calculated using Math class!\n");
 
+//a constant is a data storage item that has a programmer defined name
+//                                       has a datatype
+//                                       has an assigned value
+//a constant, unlike a variable, CANNOT be altered
+//a constant can be used in your code just like a variable
+const double POWER_OF_TWO = 2.0;
+
 double height, baseLength, hypotenuse;
 string inputValue;
+double measurementsSquared;
 
 Console.Write("Enter your height:\t");
 inputValue = Console.ReadLine();
@@ -17,6 +25,16 @@ baseLength = double.Parse(inputValue);
 //* and / left to right
 //+ and - left to right
 
-hypotenuse = Math.Sqrt(height * height + Math.Pow(baseLength, 2));
+//measurementsSquared = height * height;
+//measurementsSquared = measurementsSquared + Math.Pow(baseLength, POWER_OF_TWO);
+//hypotenuse = Math.Sqrt(measurementsSquared);
 
-Console.WriteLine($"\nThe hypotenuse of height: {height} and base {baseLength}) is: {hypotenuse.ToString("F2")}");
+//measurementsSquared = height * height;
+//measurementsSquared += Math.Pow(baseLength, POWER_OF_TWO);
+//hypotenuse = Math.Sqrt(measurementsSquared);
+
+//Refactoring
+hypotenuse = Math.Sqrt((height * height) + (Math.Pow(baseLength, POWER_OF_TWO)));
+
+
+Console.WriteLine($"\nThe hypotenuse of height: {height} and base {baseLength} is: {hypotenuse.ToString("F2")}");
