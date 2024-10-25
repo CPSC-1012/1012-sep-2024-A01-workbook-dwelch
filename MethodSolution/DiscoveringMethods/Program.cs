86d84b2dc7dc6a8e3997805e1c61a4b09a1533bd
Don Welch <dwelch@nait.ca>
2024-10-25 14:46:30 -0600

    converted parallel array program into methods.

--- MethodSolution/DiscoveringMethods/Program.cs
+++ MethodSolution/DiscoveringMethods/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\n\tParallel Arrays\n");
+Console.WriteLine("\n\tParallel Arrays Using Methods\n");
 
+#region Driver Routine
 const int PYHSICALSIZE = 25;
 int logicalSize = 0;
 
@@ -24,8 +25,9 @@ try
 {
     do
     {
-        Console.Write("Enter the student name:\t");
-        inputValue = Console.ReadLine();
+        //this method passes no parameters but receives a valid from the method
+        //  as the method completes.
+        inputValue = GetStudentName();
         if (!string.IsNullOrWhiteSpace(inputValue))
         {
             anotherStudent = true;
@@ -37,7 +39,6 @@ try
             inputValue = Console.ReadLine();
             marks[logicalSize] = int.Parse(inputValue);
             logicalSize++; //don't forget to increment
-            DisplayClass(names, marks, logicalSize);
         }
         else
         {
@@ -50,86 +51,146 @@ catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
+
+//how to sort the arrays and keep the relationship between the two arrays
+Array.Sort(names, marks, 0, logicalSize);
+
+
 //call the method that will display my data
 //this method returns nothing
-//syntax  [variable = ] methodname([list of arguments]);
+//syntax  [returnvariablename = ] methodname([list of arguments]);
+//syntax of the argument list
+//  value [, value [, ...]]
+//Rules: for your argument list
+//  value is any valid C# value
+//  the order of values MUST match the expectd order defined by the method
+//  the value MUST be of the expected datatype of the parameter defined by the method
 DisplayClass(names, marks, logicalSize);
+CalculateAverage(logicalSize, marks);
+HighestStudentMark(marks, names);
+int lowestIndex = GetLowestMarkIndex(logicalSize, marks);
+Console.WriteLine($"{names[lowestIndex]} has the lowest mark of {marks[lowestIndex]}");
+#endregion
 
+
+#region Methods
 //task of the method: display of the contents of an array collection
 //syntax of the method 
 //  access modifier returndatatype methodname([parameter list])
 //  {  ... coding block ... }
+//syntax of the parameter list
+//   datatype localvariablename [, datatype localvariablename [, ....]]
+//
+//all locally declared variables within the method exist ONLY as long as the
+//      method is executing
+//
 //if the method does not return any value then the returndatatype is: void
 //if the method does not return any value then there is NO return statement
 //if the method does return a single value then the returndatatype is of the value
 //if the method does return a value then the method MUST have a return statement
 
-//FOR THIS COURSE AS A CLASS STANDARD ALL METHOD WILL BE STATIC !!!!!!!
+//FOR THIS COURSE AS A CLASS STANDARD ALL METHODS WILL BE STATIC !!!!!!!
+
+//created a method called void DisplayClass(string[] myStrings, int[] numbers, int arraySize)
+//this declares your contract for the method
+//method header is the fully qualified method definition
+//method signature is the methodname and list of parameters
 
 static void DisplayClass(string[] myStrings, int[] numbers, int arraySize)
-{ 
+{
+   
+    //any variable that is required for this code must be declared within the method
+    //any variable that is declared within the method exists ONLY as long as the
+    //  method executes
+    //consider any parameter variable as a locally declared variable
     Console.WriteLine("\nClass list with marks\n");
     for (int index = 0; index < arraySize; index++)
     {
         Console.WriteLine($"Student: {myStrings[index]} has a mark of {numbers[index]}");
     }
+
+    //this method returns nothing (void), therefore there is NO return statement
 }
 
-//calculate the class mean average
-int sumOfMarks = 0;
-double meanAverage = 0.0;
-int classIndex = 0;
-while (classIndex < logicalSize)
+static void CalculateAverage(int logicalSize, int[] marks)
 {
-    sumOfMarks += marks[classIndex];
-    classIndex++; //classIndex += 1;
-}
-meanAverage = (double)sumOfMarks / (double)logicalSize;
-Console.WriteLine($"The mean average for our {logicalSize} students is " +
-    $" {meanAverage.ToString("##0.0")}");
-
-//find the student with the hightest mark
-int highestMark = marks[0]; //assume the first mark is the highest
-string topStudent = names[0];
-int counter = 0; //counter is necessary due to the fact that we are
-                 //  playing with parallel arrays
-
-//the foreach loop is used for collections
-//the foreach loop will
-//  a) traverse your collection from the first entry to the last entry
-//  b) you will use a "placeholder" identifier to represent the entry value
-//  c) you DO NOT need to create or manage an index to your array
-//syntax for the foreach loop:
-//    foreach (datatype placeholder in collectionname)
-//    { ....loop code .... }
-
-//NOTE: many times you will see that the datatype can be of type: var
-//      if "var" is used then the datatype is resolved at execution time
-//      using a actual actual datatype, the resolution is at compile time
-//      "var" takes its datatype from the collection datatype
-//      Using "var" when coding, may affect the intelli-sense for the software
-
-foreach (int theMark in marks)
-{
-    if (theMark > highestMark)
+    //calculate the class mean average
+    //the logicalSize is a local variable and is DIFFERENT then the logicalSize in the driver
+    //the mark is a local variable and is DIFFERENT the the marks in the driver
+    int sumOfMarks = 0;
+    double meanAverage = 0.0;
+    int classIndex = 0;
+    while (classIndex < logicalSize)
     {
-        topStudent = names[counter];
-        highestMark = theMark;
+        sumOfMarks += marks[classIndex];
+        classIndex++; //classIndex += 1;
     }
-    counter++; //only required due to the fact we are playing with parallel arrays
+    meanAverage = (double)sumOfMarks / (double)logicalSize;
+    Console.WriteLine($"The mean average for our {logicalSize} students is " +
+        $" {meanAverage.ToString("##0.0")}");
 }
-Console.WriteLine($"{topStudent} has the highest mark of {highestMark}");
 
-//find the student with the lowest mark
-int lowestMark = int.MaxValue; //MaxValue is the largest integer value possible
-int lowestIndex = 0;
-
-for (int index = 0; index < logicalSize; index++)
+static string GetStudentName()
 {
-    if (marks[index] < lowestMark)
-    {
-        lowestIndex = index;
-        lowestMark = marks[index];
-    }
+    Console.Write("Enter the student name:\t");
+    string inputValue = Console.ReadLine();
+
+    //since the return datatype is NOT void but an actual datatype, this method
+    //  is required to have a return statement
+    //syntax of the return statement:
+    //     return valid C# value of the expected datatype
+    return inputValue;
 }
-Console.WriteLine($"{names[lowestIndex]} has the lowest mark of {lowestMark}");
+
+static void HighestStudentMark(int[] marks, string[] names)
+{
+    //find the student with the hightest mark
+    int highestMark = marks[0]; //assume the first mark is the highest
+    string topStudent = names[0];
+    int counter = 0; //counter is necessary due to the fact that we are
+                     //  playing with parallel arrays
+
+    //the foreach loop is used for collections
+    //the foreach loop will
+    //  a) traverse your collection from the first entry to the last entry
+    //  b) you will use a "placeholder" identifier to represent the entry value
+    //  c) you DO NOT need to create or manage an index to your array
+    //syntax for the foreach loop:
+    //    foreach (datatype placeholder in collectionname)
+    //    { ....loop code .... }
+
+    //NOTE: many times you will see that the datatype can be of type: var
+    //      if "var" is used then the datatype is resolved at execution time
+    //      using a actual actual datatype, the resolution is at compile time
+    //      "var" takes its datatype from the collection datatype
+    //      Using "var" when coding, may affect the intelli-sense for the software
+
+    foreach (int theMark in marks)
+    {
+        if (theMark > highestMark)
+        {
+            topStudent = names[counter];
+            highestMark = theMark;
+        }
+        counter++; //only required due to the fact we are playing with parallel arrays
+    }
+    Console.WriteLine($"{topStudent} has the highest mark of {highestMark}");
+}
+
+static int GetLowestMarkIndex(int logicalSize, int[] marks)
+{
+    //find the student with the lowest mark
+    int lowestMark = int.MaxValue; //MaxValue is the largest integer value possible
+    int lowestIndex = 0;
+
+    for (int index = 0; index < logicalSize; index++)
+    {
+        if (marks[index] < lowestMark)
+        {
+            lowestIndex = index;
+            lowestMark = marks[index];
+        }
+    }
+    return lowestIndex;
+}
+#endregion
