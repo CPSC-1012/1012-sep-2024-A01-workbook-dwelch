5908c8542946a1709a4bac68fd90176595cb4860
Don Welch <dwelch@nait.ca>
2024-11-18 11:47:59 -0700

    introduction to classes

---
+++ ClassSamples/IntroToClasses/Dog.cs
@@ -0,0 +1,67 @@
+﻿//these are existing namespaces within the C# version
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//when you create a class within a project, by default, the
+//  namespace of your class is the project name
+
+namespace Animals
+{
+    //the defintion of the class name is
+    //a) access level
+    //     private nothing outside of the class has access to items
+    //                within the class
+    //     internal access to the class is limited to programming
+    //                  within the project
+    //     public access to the class is open to an outside user
+    //              of the class
+    //b) the key word class
+    //c) the programmer defintion name of the class
+    public class Dog
+    {
+        //declare the characteristics of a Dog
+        //each characteristic is a variable
+        //a valid  characteristic has a valid C# datatype
+        //NOTE: a class can have various datatyes
+
+        //Data Members (aka; fields, variables, ...)
+        //if you wish to encapsulate your data in a secure fashion
+        //  your characteristic have an access type of private
+        //if your characteristics is private then acces to that
+        //  piece of data will be via a property or method (behaviour)
+        //if you characteristic is public then any outside user
+        //  of the class can access the data directly
+
+        //DEPENDING on how the rest of your class is define, you
+        //  may or maynot have a specific data member to a piece of data
+
+        private string _Name;
+        private double _Age;
+        private string _OwnerFirstName;
+        private string _OwnerLastName;
+        private string _Breed;
+
+        //Behavour (aka method)
+        //mutator (aka setter)
+        public void SetName(string name)
+        {
+            _Name = name;
+        }
+        public void SetAge(double age)
+        {
+            _Age = age;
+        }
+        //accessor (aka getter)
+        public string GetName()
+        {
+            return _Name;
+        }
+        public double GetAge()
+        {
+            return _Age;
+        }
+    }
+}
---
+++ ClassSamples/IntroToClasses/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Animals;
+
+
+Console.WriteLine("\n\tHello, World of Classes\n!");
+
+//the class standard for coding classes will be
+//  each class defintion will be coding in a separate phyiscal file
+
+Dog myDog = null;
+Cat myCat = null;
+
+//create an instance of the class definition Dog
+//the instance is the object of the class
+//you may have as many instances of a class as you wish, each
+//  is unique, each holds its own values of the characteristics
+myDog = new Dog();
+
+//to access an item within a class you will use the dot (.) operator
+//to use something within a other class the "something" needs an
+//   access level of public or internal
+myDog.SetName("No");
+myDog.SetAge(4.5);
+DisplayMyPet(myDog);
+
+static void DisplayMyPet(Dog myDog)
+    
+{
+    //Dog instance contains the data value No.
+    Console.WriteLine($"My dog is called {myDog.GetName()}");
+    Console.WriteLine($"My dog is {myDog.GetAge()} years old.");
+}
